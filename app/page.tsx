d284efd3749be3228a260e7d4e7fb9eec8ed934a
salman221b <epsalmaan@gmail.com>
2025-12-06 13:33:26 +0530

    styling in title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Blare Fragrances</h1>
+      <h1 className="text-3xl font-bold text-center">Blare Fragrances</h1>
       
     </div>
   )
