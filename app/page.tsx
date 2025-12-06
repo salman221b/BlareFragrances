bc773a348c6283d60298e148e1544cbb483e6a0b
salman221b <epsalmaan@gmail.com>
2025-12-06 11:59:37 +0530

    Title changed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Welcome to My App</h1>
+      <h1 className="text-3xl font-bold underline text-center">Blare Fragrances</h1>
       
     </div>
   )
